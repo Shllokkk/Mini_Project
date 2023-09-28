511b30750c781a0e7f1e7a111d27b74a4d3e3192
Shllokkk <shllokosan23@gmail.com>
2023-09-28 19:31:55 +0530

    adding on 28/09/23

---
+++ src/JDBC.java
@@ -0,0 +1,34 @@
+// JDBC (Java DataBase Connectivity) MySQL code 
+
+import java.sql.*;                                                             // importing whole java.sql package to use its interfaces and classes
+
+public class JDBC{                                                             // main class
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{        // the main method may throw these checked exceptions 
+                                                                             
+        String url ="jdbc:mysql://localhost:3306/demoDB";                                            // if the url, username or password are incorrect
+        String username ="root";                                                                     // it may throw SQLException
+        String password ="Superfly23#";                   
+        
+        // load and register the MySQL Driver  
+        Class.forName("com.mysql.cj.jdbc.Driver");                                         // this line may throw ClassNotFoundException                           
+
+        // create connection to the database
+        Connection con =DriverManager.getConnection(url,username,password);                  
+        System.out.println("Connection established!");
+
+        String query ="update sample set email='****Statement****' wher id=1";                       // this line may throw SQLSyntaxErrorException
+
+        //create a statement
+        Statement stmt =con.createStatement();
+        // execute the statement
+        stmt.executeUpdate(query);
+        System.out.println("Statement executed!");
+
+        PreparedStatement prepstmt =con.prepareStatement("update sample set email='****PreparedStatement****' where id=2");  
+        prepstmt.executeUpdate();
+        System.out.println("PreparedStatement executed!");
+        
+        // close the connection
+        con.close();
+    }
+}
