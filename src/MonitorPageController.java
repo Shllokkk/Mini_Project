19035dab11f1f18d09fde177406cbeff559db0b6
Shllokkk <shllokosan23@gmail.com>
2023-10-07 01:15:55 +0530

    adding on 07/10/23

--- src/MonitorPageController.java
+++ src/MonitorPageController.java
@@ -1,10 +1,14 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class MonitorPageController {
     @FXML
@@ -21,20 +25,76 @@ public class MonitorPageController {
     @FXML
     private Button getfinances;
 
+    @FXML
+    private TableView<User> incometable;
+    @FXML
+    private TableView<User> expensetable;
+    @FXML
+    private TableView<User> investmenttable;
+
+    @FXML 
+    private TableColumn<User,String> incomeamtcolumn;
+    @FXML 
+    private TableColumn<User,String> incometypecolumn;
+    @FXML 
+    private TableColumn<User,String> incomedesccolumn;
+    @FXML 
+    private TableColumn<User,String> expenseamtcolumn;;
+    @FXML 
+    private TableColumn<User,String> expensetypecolumn;
+    @FXML 
+    private TableColumn<User,String> expensedesccolumn;
+    @FXML 
+    private TableColumn<User,String> investamtcolumn;
+    @FXML 
+    private TableColumn<User,String> investtypecolumn;
+    @FXML 
+    private TableColumn<User,String> investdesccolumn;
+
+    public ObservableList<User> incomelist=FXCollections.observableArrayList();
 
 
+    public void onGetFinancesButtonClick(ActionEvent event) throws Exception {
 
-
-    public void onGetFinancesButtonClick() throws Exception{
         String foreignkey=LoginPageController.foreignkey;
+        String getincomedata = "select (incomeamt,incometype,incomedesc) from userincome where uemail = '"+foreignkey+ "'";
 
-        JDBC connectnow=new JDBC();
-        Connection connectdb=connectnow.getconnection();
+        JDBC connectnow = new JDBC();
+        Connection connectdb = connectnow.getconnection();
 
-        //String datainsert="insert into userinvestment values ('"+foreignkey+"','"+typefield.getText()+"','"+amtfield.getText()+"','"+descfield.getText()+"')";
-        String getdata="select (incomeamt) from userincome where uemail='"+foreignkey+"'";
+        Statement statement = connectdb.createStatement();
+        ResultSet resultset = statement.executeQuery(getincomedata);
 
-        Statement statement=connectdb.createStatement();
-        ResultSet result=statement.executeQuery(getdata);
+        while(resultset.next()) {
+            String amount=resultset.getString("incomeamt");
+            String type=resultset.getString("incometype");
+            String desc=resultset.getString("incomedesc");
+
+            incomelist.add(new User(amount,type,desc));
+        }
+        incometable.setItems(incomelist);
+    }
+}
+
+class User {
+
+    private String amount;
+    private String type;
+    private String desc;
+
+    public User(String amount,String type,String desc) {
+        this.amount=amount;
+        this.type=type;
+        this.desc=desc;
+    }
+
+    public String getAmount() {
+        return this.amount;
+    }
+    public String getType() {
+        return this.type;
+    }
+    public String getDesc() {
+        return this.desc;
     }
 }
